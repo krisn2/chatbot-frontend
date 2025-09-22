25c1e8ab42867b777e44b95135be72ff3a78a6b3
krishna sarone <167612353+krisn2@users.noreply.github.com>
2025-09-23 02:18:54 +0530

    env checks

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API,
   withCredentials: true // IMPORTANT: backend uses cookie token
 })
 
